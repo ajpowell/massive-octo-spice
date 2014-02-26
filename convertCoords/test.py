e412886f06f2a1fd40b4d8ec3b3e6e6f9882ab20
ajpowell <adrian.powell@apmf.co.uk>
2014-02-26 20:48:04 +0000

    Added WGS84toOSGB36 function

--- convertCoords/test.py
+++ convertCoords/test.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 
 from BNG import to_osgb36, from_osgb36
-from EN2lnglat import OSGB36toWGS84
+from EN2lnglat import OSGB36toWGS84, WGS84toOSGB36
 
-ngr='NT275729'
+#ngr='NT275729'
+ngr='SP28031820'
 
 print ngr
 
 E, N = to_osgb36(ngr)
-#    (327550, 672950)
+
 print E
 print N
    
@@ -16,3 +17,8 @@ lat, lon = OSGB36toWGS84(float(E), float(N))
 
 print lat
 print lon
+
+E, N = WGS84toOSGB36(lat, lon)
+
+print E
+print N
